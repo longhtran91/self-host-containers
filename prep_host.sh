25737e6b3c844016c13b1b7c2bf28b78d280d647
Long Tran <longhtran91@gmail.com>
2023-08-14 09:00:49 -0500

    add instruction to mount iscsi

--- prep_host.sh
+++ prep_host.sh
@@ -3,3 +3,22 @@
 #builtin admin for truenas
 sudo groupadd -g 544 builtin_administrators
 sudo usermod -aG builtin_administrators "$USER"
+
+#ISCSI
+sudo apt update && sudo apt install open-iscsi
+sudo iscsiadm -m discovery -t sendtargets -p {IPADDRESS} #discover target
+sudo iscsiadm -m node -T {BASENAME}:{TARGETNAME} -p {IPADDRESS} -l #establish session
+
+sudo fdisk -l
+sudo fdisk {DISK} #e.g. /dev/sdb -> create partition
+sudo mkfs.ext4 {PARTITION_NAME} #e.g. /dev/sdb1
+
+sudo mkdir ~./compose/container-configs
+
+echo >> "{PARTITION_NAME} {HOME}/compose/container-configs ext4 defaults 0 0"
+
+
+
+
+sudo iscsiadm -m node -T {BASENAME}:{TARGETNAME} -p {IPADDRESS} -u #kill session
+sudo iscsiadm -m node -o delete -T {BASENAME}:{TARGETNAME} #remove node
